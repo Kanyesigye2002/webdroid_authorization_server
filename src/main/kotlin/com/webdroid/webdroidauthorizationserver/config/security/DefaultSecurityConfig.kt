cd177465e50ec26685a5cdccbb94aec218f69ffd
Kanyesigye2002 <kanyeallan@gmail.com>
2023-02-10 18:01:33 +0300

    v0.0.2-1

--- src/main/kotlin/com/webdroid/webdroidauthorizationserver/config/security/DefaultSecurityConfig.kt
+++ src/main/kotlin/com/webdroid/webdroidauthorizationserver/config/security/DefaultSecurityConfig.kt
@@ -22,6 +22,7 @@ import com.webdroid.webdroidauthorizationserver.config.security.oauth2.CustomOAu
 import com.webdroid.webdroidauthorizationserver.config.security.oauth2.HttpCookieOAuth2AuthorizationRequestRepository
 import com.webdroid.webdroidauthorizationserver.config.security.oauth2.OAuth2AuthenticationFailureHandler
 import com.webdroid.webdroidauthorizationserver.config.security.oauth2.OAuth2AuthenticationSuccessHandler
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -31,7 +32,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.session.HttpSessionEventPublisher
-import java.io.File
+import org.springframework.util.ResourceUtils
 import java.io.IOException
 
 /**
@@ -51,6 +52,8 @@ class DefaultSecurityConfig @Autowired constructor(
     private val oAuth2AuthenticationSuccessHandler: OAuth2AuthenticationSuccessHandler,
     private val oAuth2AuthenticationFailureHandler: OAuth2AuthenticationFailureHandler
 ) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @Bean
     @Throws(Exception::class)
@@ -134,7 +137,9 @@ class DefaultSecurityConfig @Autowired constructor(
         GeoIp2Exception::class
     )
     fun databaseReader(): DatabaseReader? {
-        val resource = File("src/main/resources/maxmind/GeoLite2-Country.mmdb")
+        val resource = ResourceUtils.getFile("classpath:maxmind/GeoLite2-Country.mmdb")
+//        val resource = File("src/main/resources/maxmind/GeoLite2-Country.mmdb")
+        logger.info("GeoIPCity: ${resource.absolutePath}")
         return DatabaseReader.Builder(resource).build()
     }
 
--- src/main/kotlin/com/webdroid/webdroidauthorizationserver/config/security/LoginNotificationConfig.kt
+++ src/main/kotlin/com/webdroid/webdroidauthorizationserver/config/security/LoginNotificationConfig.kt
@@ -4,8 +4,8 @@ import com.maxmind.geoip2.DatabaseReader
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.util.ResourceUtils
 import ua_parser.Parser
-import java.io.File
 import java.io.IOException
 
 @Configuration
@@ -20,13 +20,17 @@ class LoginNotificationConfig {
     @Bean(name = ["GeoIPCity"])
     @Throws(IOException::class)
     fun databaseReader(): DatabaseReader {
-        val resource = File("src/main/resources/maxmind/GeoLite2-City.mmdb")
-        val resource2 = File("src/main/resources/maxmind/GeoLite2-Country.mmdb")
-        logger.info("GeoIPCity: ${resource.absolutePath}")
-        logger.info("GeoIPCountry: ${resource2.absolutePath}")
-//        val database = ResourceUtils
-//            .getFile("classpath:maxmind/GeoLite2-City.mmdb")
-        return DatabaseReader.Builder(resource)
+//        val resource = File("src/main/resources/maxmind/GeoLite2-City.mmdb")
+//        val resource2 = File("src/main/resources/maxmind/GeoLite2-Country.mmdb")
+//        logger.info("GeoIPCity: ${resource.absolutePath}")
+//        logger.info("GeoIPCountry: ${resource2.absolutePath}")
+        val database = ResourceUtils
+            .getFile("classpath:maxmind/GeoLite2-City.mmdb")
+        val database2 = ResourceUtils
+            .getFile("classpath:maxmind/GeoLite2-Country.mmdb")
+        logger.info("GeoIPCity database: ${database.absolutePath}")
+        logger.info("GeoIPCountry database: ${database2.absolutePath}")
+        return DatabaseReader.Builder(database)
             .build()
     }
 }
